1aeae6925359399d21617d26950a1f820c0b0553
Moe Aydin <maydin@zipcodes-MacBook-Pro-7.local>
2020-02-05 18:08:37 -0500

    wrote methods for MultiplicationTable

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,34 @@
- 
+
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+      String result = "";
+      result = getMultiplicationTable(5);
+      return result;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String result = "";
+        result = getMultiplicationTable(10);
+        return result;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+        for (int row = 1 ; row <= tableSize ; row++) {
+            for (int col = 1 ; col <= tableSize ; col++) {
+                if ( row * col < 10) {
+                  result += "  " + row*col + " |";
+                }
+                else if ( (row*col >= 10) && (row*col <100) ) {
+                  result += " " + row*col + " |";
+                }
+                else {
+                  result += row*col + " |";
+                }
+            }
+            result += "\n";
+        }
+        return result;
     }
 }
