755418581c7493035ce075faa2e2d48981b272d0
Moe Aydin <maydin@zipcodes-MacBook-Pro-7.local>
2020-02-05 16:21:28 -0500

    completed numberUtilities portion

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,62 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String result = "";
+        for (int i = 0; i < stop ; i++) {
+            result += i;
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+          result += i;
+        }
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+          result += i;
+        }
+        return result;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop ; i++){
+            if (i % 2 == 0){
+              result += i;
+            }
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+      String result = "";
+      for (int i = start; i < stop ; i++){
+          if (i % 2 == 1){
+            result += i;
+          }
+      }
+      return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String result = "";
+        int exponentValue = 1;
+        for (int i = start; i <= stop; i++) {
+            exponentValue = 1;
+            for (int j = 1; j <= exponent; j++) {
+              exponentValue *= i;
+            }
+            result += exponentValue;
+        }
+        return result;
     }
 }
